0edfd5ecac25aba9c2a3aba6d65e71bc0b48502c
nogamate <kurimotoishere@gmail.com>
2026-05-06 06:15:31 +0900

    Auto commit 2026/05/06  6:15:30.97

--- send_line.py
+++ send_line.py
@@ -25,7 +25,7 @@ def send_monthly_broadcast():
     with sync_playwright() as p:
         # ブラウザを起動 (headless=False で動作が見えるようにします)
         print("ブラウザを起動中...")
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context(locale="ja-JP")
         page = context.new_page()
 
